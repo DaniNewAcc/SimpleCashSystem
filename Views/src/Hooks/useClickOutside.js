68402ee3b41e595406d192034e6c8268faf2a733
Daniele <ddaaaniii.98@gmail.com>
2025-12-02 23:58:49 +0100

    refactor(hooks): update useClickOutside hook logic

--- Views/src/Hooks/useClickOutside.js
+++ Views/src/Hooks/useClickOutside.js
@@ -1,19 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from 'react';
 
-export function useClickOutside(ref, onClickOutside){
-    useEffect(() => {
-        function handleClickOutside(event) {
-            if(ref.current && !ref.current.contains(event.target)) {
-                onClickOutside();
-            }
-        }
-      
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("touchstart", handleClickOutside);
+export function useClickOutside(onClickOutside, enabled = true) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (!enabled) return;
+
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClickOutside();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.removeEventListener("touchstart", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
     };
-  }, [ref, onClickOutside]);
+  }, [onClickOutside, enabled]);
+
+  return ref;
 }
